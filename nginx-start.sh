faf56cfb6b9c8ca894b6d8ec171cd44c0150d681
Phil Weir <phil.weir@flaxandteal.co.uk>
2016-08-25 18:33:57 +0100

    update for kubernetes

--- nginx-start.sh
+++ nginx-start.sh
@@ -2,6 +2,6 @@
 
 #With thanks to dylanlindgren
 cp /opt/etc/nginx.conf /etc/nginx/nginx.conf
-sed -i "s/%fpm-ip%/$FPM_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
+sed -i "s/%fpm-ip%/$PHPFPM_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
 sed -i "s/LARAVEL_SERVER_NAME/$LARAVEL_SERVER_NAME/g" /etc/nginx/sites-available/laravel
 exec /usr/sbin/nginx
