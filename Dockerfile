a65500ec0b76af3d546255a2d651473859f3368f
Phil Weir <phil.weir@flaxandteal.co.uk>
2016-08-31 22:03:37 +0100

    update for kubernetes and gitlab ci

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
 FROM dylanlindgren/docker-laravel-nginx
 
 RUN rm -rf /etc/nginx/sites-available/laravel
+RUN apt-get update
+RUN apt-get install -y git
 #RUN sed -i 's/\(include \/etc\/nginx\/sites-enabled\/\*;\)/\1\n include \/data\/*.conf;/g' /etc/nginx/nginx.conf
 
 ADD laravel /etc/nginx/sites-available/
-ADD nginx-start.sh /opt/bin/nginx-start.sh
+COPY nginx-start.sh /opt/bin/nginx-start.sh
+VOLUME ["/var/www/app"]
 RUN chmod u=rwx /opt/bin/nginx-start.sh
--- nginx-start.sh
+++ nginx-start.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+mkdir -p /data/logs
+chown -R www-data /data/logs
+
 #With thanks to dylanlindgren
 cp /opt/etc/nginx.conf /etc/nginx/nginx.conf
-sed -i "s/%fpm-ip%/$PHPFPM_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
+
+if [ -z "$PHPFPM_PORT_9000_TCP_ADDR" ]
+then
+  sed -i "s/%fpm-ip%/$PHPFPM_1_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
+else
+  sed -i "s/%fpm-ip%/$PHPFPM_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
+fi
 sed -i "s/LARAVEL_SERVER_NAME/$LARAVEL_SERVER_NAME/g" /etc/nginx/sites-available/laravel
+
+
 exec /usr/sbin/nginx
